a381e34e6c1e0bebc1bea344e9befac2770212df
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-02-05 21:03:15 +0700

    fix logout

--- be-tunam8/app/Http/Controllers/AuthController.php
+++ be-tunam8/app/Http/Controllers/AuthController.php
@@ -33,7 +33,7 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        $request->user()->token()->revoke();
+        $request->user()->currentAccessToken()->delete();
         return response()->json(['message' => 'Successfully logged out'], 200);
     }
 
