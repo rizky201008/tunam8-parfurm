3ebf056a9da4f107e3dd79f055abc5103386cd5f
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-02-06 15:37:54 +0700

    create new middleware

---
+++ be-tunam8/app/Http/Middleware/CheckAbility.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAbility
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string $ability): Response
+    {
+        if (!$request->user()->tokenCan($ability)) {
+            return response()->json(['message' => 'Forbidden'], 403);
+        }
+
+        return $next($request);
+    }
+}
