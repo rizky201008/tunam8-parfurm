4aa4be18a4e3434f1bbd0b012ad2b9cb68125b82
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-02-06 15:21:05 +0700

    modify Product model

--- be-tunam8/app/Models/Product.php
+++ be-tunam8/app/Models/Product.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'category_id',
+        'price',
+        'stock',
+        'description',
+        'images',
+        'slug',
+    ];
 }
