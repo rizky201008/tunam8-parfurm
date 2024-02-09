3d8f238bc959be599f6a6cbd5374a14581f9a48d
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-02-10 02:59:02 +0700

    Add seeders for Address, Category, and User models

--- be-tunam8/database/seeders/DatabaseSeeder.php
+++ be-tunam8/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Address;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Transaction;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        Address::factory(10)->create();
+        Category::factory(10)->create();
+        // Product::factory(10)->create();
+        // Transaction::factory(100)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
