571496e403b28993aff31a8efc3dfd1907765606
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-02-13 22:37:27 +0700

    Refactor UserController and add user personalization

--- be-tunam8/app/Http/Controllers/UserController.php
+++ be-tunam8/app/Http/Controllers/UserController.php
@@ -3,11 +3,54 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\UserPersonal;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function getAllUsers(){
-        return response()->json(['users' => User::all()], 200);
+    public function getAllUsers()
+    {
+        return response()->json(User::all(), 200);
+    }
+
+    public function personalizeUser(Request $request)
+    {
+        $validated = $request->validate([
+            'tags' => 'required|array',
+        ]);
+
+        $validated['tags'] = json_encode($validated['tags']);
+
+        $personal = UserPersonal::where('user_id', $request->user()->id)->first();
+        if ($personal === null) {
+            $createdPersonal = UserPersonal::create([
+                'user_id' => $request->user()->id,
+                'tags' => $validated['tags'],
+            ]);
+
+            return response()->json(['personal' => $createdPersonal, 'message' => 'User personalization created'], 201);
+        }
+
+        return response()->json(['personal' => $personal, 'message' => 'User personalization already exists'], 200);
+    }
+
+    public function getUserPersonal(Request $request)
+    {
+        $user = User::find($request->user()->id);
+        $personal = UserPersonal::where('user_id', $request->user()->id)->first();
+        return response()->json(['personal' => $user->personal], 200);
+    }
+
+    public function updateUserPersonal(Request $request)
+    {
+        $validated = $request->validate([
+            'tags' => 'required|array',
+        ]);
+
+        $user = User::find($request->user()->id);
+        $user->personal->tags = $validated['tags'];
+        $user->personal->save();
+
+        return response()->json(['personal' => $user->personal, 'message' => 'User personalization updated'], 200);
     }
 }
