8d593ad489426e6354b09f643f1b5694d7f48544
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-02-15 12:32:40 +0700

    Add CartItem model

---
+++ be-tunam8/app/Models/CartItem.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class CartItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'cart_id',
+        'product_id',
+        'quantity',
+        'user_id',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
