a98a8511099421e57a5bc44296bb784d487cb5ec
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-02-15 12:31:41 +0700

    Add CartItem model to DatabaseSeeder

--- be-tunam8/database/seeders/DatabaseSeeder.php
+++ be-tunam8/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Address;
+use App\Models\CartItem;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\Transaction;
@@ -17,9 +18,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
-        Address::factory(10)->create();
-        Category::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
+        // Address::factory(10)->create();
+        // Category::factory(10)->create();
+        CartItem::factory(10)->create();
         // Product::factory(10)->create();
         // Transaction::factory(100)->create();
 
