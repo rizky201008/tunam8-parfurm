89e0920a90cad08976a4e152c6e832d796f1a267
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-02-18 00:12:39 +0700

    feat: create new models

---
+++ be-tunam8/app/Models/Province.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Province extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'province',
+        'province_id'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'id'
+    ];
+}
