f3a9f4bfac4f896b4d220dbe36937f7def102fa3
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-02-18 20:00:59 +0700

    refactor: add fillable and hidden

--- be-tunam8/app/Models/Address.php
+++ be-tunam8/app/Models/Address.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'province',
+        'province_id',
+        'city',
+        'city_id',
+        'postal_code',
+        'address_detail',
+        'phone_number',
+        'receiver',
+        'label',
+        'is_primary',
+        'user_id'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'user_id'
+    ];
 }
