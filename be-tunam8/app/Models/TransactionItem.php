c99ee7340993604d1eefa10d4697dcccd922c60a
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-02-19 16:33:24 +0700

    feat: db: create new migration and model

---
+++ be-tunam8/app/Models/TransactionItem.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class TransactionItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'transaction_id',
+        'product_id',
+        'price',
+        'qty'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+}
---
+++ be-tunam8/database/migrations/2024_02_19_073253_create_transaction_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Product;
+use App\Models\Transaction;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Transaction::class);
+            $table->foreignIdFor(Product::class);
+            $table->double('price');
+            $table->double('qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction_items');
+    }
+};
