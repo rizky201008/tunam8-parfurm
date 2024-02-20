1f1a52a7a181d44c4978229c6b17bf4693253578
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-02-20 08:54:50 +0700

    refactor: db: modify table fields

--- be-tunam8/database/migrations/2024_02_15_025757_create_cart_items_table.php
+++ be-tunam8/database/migrations/2024_02_15_025757_create_cart_items_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->foreignIdFor(User::class);
             $table->foreignIdFor(Product::class);
             $table->integer('quantity');
+            $table->boolean('selected')->default(false);
             $table->timestamps();
         });
     }
