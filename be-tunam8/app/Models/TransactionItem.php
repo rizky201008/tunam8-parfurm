f63432b932096815a757b913f8741bcfd87d8b30
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-02-24 15:45:48 +0700

    feat: add hidden value

--- be-tunam8/app/Models/TransactionItem.php
+++ be-tunam8/app/Models/TransactionItem.php
@@ -17,6 +17,11 @@ class TransactionItem extends Model
         'qty'
     ];
 
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
     public function product()
     {
         return $this->belongsTo(Product::class);
