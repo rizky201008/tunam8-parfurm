6c4676c0316aafea7df31a7a265e672503818ca6
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-04-28 20:37:16 +0700

    modify accessors and add fillable

--- be-tunam8/app/Models/ProductImage.php
+++ be-tunam8/app/Models/ProductImage.php
@@ -2,16 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductImage extends Model
 {
     use HasFactory;
-    protected $fillable = ['product_id', 'link'];
+    protected $fillable = ['product_id', 'link', 'path'];
     protected $hidden = ['created_at', 'updated_at', 'product_id', 'id'];
-    public function getLinkAttribute($value)
+    protected function link(): Attribute
     {
-        return config('app.url') . '/' . 'products/' . $value;
+        return Attribute::make(
+            get: fn (string $value) => config('app.url') . $value,
+        );
     }
 }
