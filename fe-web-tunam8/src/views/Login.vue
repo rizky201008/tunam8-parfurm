a927656c1baab402832acc01b22171224036bc34
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-08 17:59:46 +0700

    modify Login.vue

--- fe-web-tunam8/src/views/Login.vue
+++ fe-web-tunam8/src/views/Login.vue
@@ -255,9 +255,7 @@ export default {
                 const response = await axios.post(BASE_URL + '/account/login', {
                     email: this.loginEmail,
                     password: this.loginPassword
-                },{
-                  headers : {"Access-Control-Allow-Origin": "*"}
-              });
+                });
 
                 // your success handling code
                 this.$notify({
