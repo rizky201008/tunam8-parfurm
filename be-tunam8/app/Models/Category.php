1946c5dfd2fff11694d1bab7ce033d277d156c4e
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-10 10:25:26 +0700

    add new relation

--- be-tunam8/app/Models/Category.php
+++ be-tunam8/app/Models/Category.php
@@ -11,4 +11,9 @@ class Category extends Model
 
     protected $hidden = ['created_at', 'updated_at'];
     protected $fillable = ['name', 'slug'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
