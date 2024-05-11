2174350868b4d1a8e863f102f49bc76bc6bb4084
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-11 23:20:37 +0700

    modify api routes

--- be-tunam8/routes/api.php
+++ be-tunam8/routes/api.php
@@ -36,8 +36,7 @@ Route::group(['prefix' => 'account'], function () {
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('products', [ProductController::class, 'allProducts']);
     Route::get('product/{slug}', [ProductController::class, 'getProduct']);
-    Route::post('search-products', [ProductController::class, 'searchProductByName']);
-    Route::get('search-products-category', [ProductController::class, 'searchProductByCategory']);
+    Route::get('search-products', [ProductController::class, 'searchProductByName']);
     Route::get('personalized-products', [ProductController::class, 'getPersonalizedProducts']);
     Route::get('categories', [CategoryController::class, 'allCategories']);
     Route::get('category/{slug}', [CategoryController::class, 'getCategoryBySlug']);
