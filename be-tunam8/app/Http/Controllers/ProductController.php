eda2a377a9c06ed96439c46ae8d502cd957d7ed1
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-25 16:29:22 +0700

    modify getPersonalizedProducts function

--- be-tunam8/app/Http/Controllers/ProductController.php
+++ be-tunam8/app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Logics\ProductRepository;
 use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
@@ -19,19 +20,32 @@ class ProductController extends Controller
 
     public function getPersonalizedProducts(Request $request)
     {
-        $tags = $request->user()->personal;
+        $user = $request->user();
+        $tags = $user->personal;
         $explodedTags = explode(',', $tags->tags);
         $filteredProduct = [];
-        foreach ($explodedTags as $tag) {
-            $tag = Str::replace(' ', '', $tag);
-            $products = Product::with(['category', 'images'])->where('tags', 'LIKE', "%$tag%")->inRandomOrder()->limit(5)->get();
-            foreach ($products as $product) {
-                $filteredProduct[] = $product;
+
+        $jenis_kelamin = $user->jenis_kelamin ?? 'u';
+        $query = Product::with(['category', 'images'])->where('jenis_kelamin', $jenis_kelamin);
+
+        if (!empty($explodedTags)) {
+            foreach ($explodedTags as $tag) {
+                $tag = Str::replace(' ', '', $tag);
+                $products = $query->where('tags', 'LIKE', "%$tag%")->first();
+                if ($products !== null) {
+                    $filteredProduct[] = $products;
+                }
+            }
+        } else {
+            $products = $query->get();
+            if ($products->isNotEmpty()) {
+                $filteredProduct = $products->toArray();
             }
         }
 
-        return response()->json($filteredProduct);
+        return response()->json(array_slice($filteredProduct, 0, 5));
     }
+
 
     public function createProduct(Request $request)
     {
@@ -158,6 +172,6 @@ class ProductController extends Controller
             $products = Product::with(['category', 'images'])->where('name', 'LIKE', "%$name%")->where('category_id', $category)->get();
         }
 
-            return response()->json($products);
+        return response()->json($products);
     }
 }
